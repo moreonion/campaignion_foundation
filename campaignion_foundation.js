40555f062ce6726264246a64008c5c0e5a9c41b0
Katharina Zwinger <katharina@more-onion.com>
2019-11-08 11:09:59 +0100

    Overwrite forms to work with Foundation: validation
    Add custom function for clientside validation:
    `campaignion_foundation_errors`

---
+++ campaignion_foundation.js
@@ -0,0 +1,52 @@
+/**
+ * @file Drupal behaviors for the Campaignion Foundation Theme.
+ */
+
+(function ($) {
+
+Drupal.behaviors.campaignion_foundation_clientside_validation = {};
+Drupal.behaviors.campaignion_foundation_clientside_validation.attach = function (context, settings) {
+
+  // Helper function for finding the top/last wrapper of radio and checkbox fields.
+  function findWrapper ($element) {
+    if (!($element.is(':radio') || $element.is(':checkbox'))) {
+      return $element;
+    }
+    var $parents = $element.parents('.form-radios, .form-checkboxes');
+    if (!$parents.length) {
+      $parents = $element.parents('.form-type-radio, .form-type-checkbox').siblings().addBack().last();
+    }
+    if (!$parents.length) {
+      $parents = $element;
+    }
+    return $parents;
+  }
+
+  // Add classes to error message and place it after the form field.
+  $('form', context).on('clientsideValidationInitialized', function() {
+    // Register custom error function with clientside validation.
+    Drupal.myClientsideValidation['campaignion_foundation_errors'] = function (error, element) {
+      $(error).addClass('form-error is-visible');
+      if ($(element).is(':radio') || $(element).is(':checkbox')) {
+        $(error).css('margin-top', 0);
+      }
+      $(error).insertAfter(findWrapper($(element)));
+    }
+  });
+
+  // Add classes to form elements.
+  $('form', context).on('clientsideValidationInvalid', function (event, element) {
+    var $wrapper = findWrapper($(element));
+    $(element).addClass('is-invalid-input');
+    $wrapper.siblings('label').addClass('is-invalid-label');
+    $wrapper.next('.form-error').addClass('is-visible');
+  });
+  $('form', context).on('clientsideValidationValid', function (event, element) {
+    var $wrapper = findWrapper($(element));
+    $(element).removeClass('is-invalid-input');
+    $wrapper.siblings('label').removeClass('is-invalid-label');
+    $wrapper.next('.form-error').removeClass('is-visible');
+  });
+};
+
+})(jQuery);
