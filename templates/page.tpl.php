27f5ca603ea715e25e62f3ad17e3c34531f78c37
Katharina Zwinger <katharina@more-onion.com>
2021-03-03 16:22:24 +0100

    Move form blocks to sidebar first

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -125,12 +125,10 @@
     <?php if ($layout === 'cover-banner'): ?>
     <section id="banner-content">
       <div class="grid-container with sidebar">
-        <?php if ($existing_form_blocks = array_intersect($form_blocks, array_keys($page['sidebar_second']))): ?>
+        <?php if (!empty($page['sidebar_first'])): ?>
         <div id=form-wrapper class="flex-container align-middle">
           <div id="form-outer">
-            <?php foreach ($existing_form_blocks as $block): ?>
-              <?php print render($page['sidebar_second'][$block]); ?>
-            <?php endforeach; ?>
+            <?php print render($page['sidebar_first']); ?>
           </div>
         </div>
         <?php endif; ?>
@@ -139,7 +137,7 @@
     <?php endif; ?>
 
     <section id="main">
-      <div class="grid-container<?php print ($is_narrow ? ' narrow' : ''); ?><?php print (!$is_narrow && $has_sidebar ? ' with-sidebar' : ''); ?>">
+      <div class="grid-container<?php print ($is_narrow ? ' narrow' : ''); ?><?php print ($has_sidebar ? ' with-sidebar' : ''); ?>">
 
         <?php if ($layout === 'cover-1col'): ?><div class="inner-wrapper"><?php endif; ?>
 
@@ -163,19 +161,23 @@
           </div>
 
           <?php if ($has_sidebar && $layout !== 'cover-banner'): ?>
-            <?php if (!$is_narrow): ?><div id="sidebar"><?php endif; ?>
-              <?php print render($page['sidebar_first']); ?>
-              <?php if ($existing_form_blocks = array_intersect($form_blocks, array_keys($page['sidebar_second']))): ?>
+            <div id="sidebar">
+              <?php if (!empty($page['sidebar_first'])): ?>
                 <div id=form-wrapper class="flex-container align-middle">
                   <div id="form-outer">
-                    <?php foreach ($existing_form_blocks as $block): ?>
-                      <?php print render($page['sidebar_second'][$block]); ?>
-                    <?php endforeach; ?>
+                    <?php print render($page['sidebar_first']); ?>
                   </div>
                 </div>
               <?php endif; ?>
               <?php print render($page['sidebar_second']); ?>
-            <?php if (!$is_narrow): ?></div><?php endif; ?>
+            </div>
+          <?php elseif ($layout === 'cover-1col'): ?>
+            <?php if (!empty($page['sidebar_first'])): ?>
+            <div id="form-outer">
+              <?php print render($page['sidebar_first']); ?>
+            </div>
+            <?php endif; ?>
+            <?php print render($page['sidebar_second']); ?>
           <?php endif; ?>
 
           <div id="content">
@@ -193,9 +195,8 @@
             <?php endif; ?>
           </div>
 
-          <?php if ($has_sidebar && $layout === 'cover-banner'): ?>
+          <?php if ($layout === 'cover-banner' && $has_sidebar): ?>
           <div id="sidebar">
-            <?php print render($page['sidebar_first']); ?>
             <?php print render($page['sidebar_second']); ?>
           </div>
           <?php endif; ?>
