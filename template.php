ba657c1c2b1b90de6118910dd7453b95d87c1e7c
Roman Zimmermann <torotil@gmail.com>
2025-04-09 14:39:35 +0200

    Revert "d7csp: Don’t assume theme is active when generating d7cps hosts"

--- template.php
+++ template.php
@@ -112,7 +112,7 @@ function campaignion_foundation_preprocess_html(&$vars) {
  * Implements hook_d7csp_hosts_alter().
  */
 function campaignion_foundation_d7csp_hosts_alter(&$hosts) {
-  $css_path = theme_get_setting('foundation_assets_css', 'campaignion_foundation');
+  $css_path = theme_get_setting('foundation_assets_css');
   if ($host = parse_url($css_path, PHP_URL_HOST)) {
     $hosts['style-src'][] = $host;
     $hosts['font-src'][] = $host;
