889f1b8cd1e856d5b40ffcb3e38d9417e3214f4f
Roman Zimmermann <roman@more-onion.com>
2025-11-12 10:16:31 +0100

    doc(template.php): Add comment for hacky page flag

--- template.php
+++ template.php
@@ -268,6 +268,7 @@ function campaignion_foundation_preprocess_file_entity(&$vars) {
   }
   // Remove heading for screen readers.
   if (in_array('element-invisible', $vars['title_attributes_array']['class'] ?? [])) {
+    // Simple but hacky way to make file_entity.tpl.php not render the title.
     $vars['page'] = TRUE;
   }
 }
